19797d470c5e93f12014813651d2d29bbdf2066b
bonacc <58808442+bonacc@users.noreply.github.com>
2020-04-07 10:43:05 +0200

    Lezione 4

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,11 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe("Test Recipe", "Simple Description Test", "https://i.imgur.com/fMdnwbd.jpg%22"),
+    new Recipe("Test", "Descrizione", "https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2019/11/Come-pastorizzare-le-uova-7.jpg?w=580"),
+    new Recipe("", "", ""),
+    new Recipe("", "", ""),
+    new Recipe("", "", ""),
+    new Recipe("", "", ""),
     new Recipe("", "", ""),
     new Recipe("", "", "")
   ];
