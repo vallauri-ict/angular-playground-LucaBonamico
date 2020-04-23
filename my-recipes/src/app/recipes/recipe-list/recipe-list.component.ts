d055b77e0bbccf15ee4ddb36b5ea8c1d6f9c01c4
bonacc <58808442+bonacc@users.noreply.github.com>
2020-04-23 09:19:05 +0200

    Lezione 6

--- my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ my-recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { DataStorageService } from 'src/app/shared/data-storage.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,17 +11,16 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe("Test", "Descrizione", "https://images.fidhouse.com/fidelitynews/wp-content/uploads/sites/6/2019/11/Come-pastorizzare-le-uova-7.jpg?w=580"),
-    new Recipe("", "", ""),
-    new Recipe("", "", ""),
-    new Recipe("", "", ""),
-    new Recipe("", "", ""),
-    new Recipe("", "", ""),
     new Recipe("", "", "")
   ];
 
-  constructor() { }
+  constructor(private dataStorageservice: DataStorageService) { }
 
   ngOnInit(): void {
+    this.dataStorageservice.sendGetRequest('recipes').subscribe((data:any[])=>{
+      console.log(data);
+      this.recipes=data;
+    })
   }
 
 }
